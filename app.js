41f1384fabc637642d6ce8310af93f130606666e
iamsabby <sohailsabby@gmail.com>
2024-07-03 20:47:59 +0500

    1st Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+AOS.init({
+    offset: 120,
+    delay: 0,
+    duration: 900,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: "top-bottom",
+  });
+
+  // document.addEventListener("DOMContentLoaded", function () {
+  //   var lastScrollTop = 0;
+  //   var navbar = document.getElementById("navbar");
+  
+  //   window.addEventListener("scroll", function () {
+  //     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+  //     if (scrollTop > lastScrollTop) {
+  //       // Scrolling down
+  //       navbar.classList.add("hidden");
+  //     } else {
+  //       // Scrolling up
+  //       navbar.classList.remove("hidden");
+  //     }
+  
+  //     lastScrollTop = scrollTop;
+  //   });
+  // });
+  
